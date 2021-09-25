129b971268572c35057a62ef755d78dec030bfb9
PaulOchoa952 <75690746+PaulOchoa952@users.noreply.github.com>
2021-09-24 19:27:32 -0600

    Add files via upload

---
+++ TAP_U1_P3/src/ProyectoException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author p_a_u
+ */
+public class ProyectoException extends Exception {
+
+    public ProyectoException(String message) {
+    super(message);
+    }
+
+  
+    
+}
